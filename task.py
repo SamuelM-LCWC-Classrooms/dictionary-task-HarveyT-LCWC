7ce6c406728608a9b8f2e0f505a01bacbc270e3d
HarveyT-LCWC <144792010+HarveyT-LCWC@users.noreply.github.com>
2024-09-16 12:47:09 +0000

    boo

--- task.py
+++ task.py
@@ -3,5 +3,9 @@ def task(input): # the value of input is an integer which is the total money ava
     result = {} # Empty dictionary
 
     # Code here...
-
+ = {
+    "Needs": input * 0.5
+    "Wants": input * 0.3
+    "savings":input * 0.2
     return result # result should be a dictionary wiht the correct values
+    
